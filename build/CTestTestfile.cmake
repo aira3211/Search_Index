41071ffa86d237eb6197b33d3e622e3f03d488b4
aria <aryashahbazi@gmail.com>
2021-03-23 00:11:22 +0430

    Done

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/Search_Index/Search_Index
+# Build directory: G:/C++/AW/Search_Index/Search_Index/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
